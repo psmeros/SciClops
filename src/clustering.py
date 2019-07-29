6091428c0f46284fbb0ec3673c857beade076ec4
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-07-29 22:25:39 +0200

    Apriori alg for references.

--- src/clustering.py
+++ src/clustering.py
@@ -1,24 +1,67 @@
+import re
+from itertools import combinations
 from pathlib import Path
 
+import networkx as nx
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from utils import read_graph
-import networkx as nx
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
 
 
-scilens_dir = str(Path.home()) + '/Dropbox/scilens/'
-# hn_vocabulary = open(scilens_dir + 'small_files/hn_vocabulary/hn_vocabulary.txt').read().splitlines()
+from utils import analyze_url, read_graph
+
+############################### CONSTANTS ###############################
+scilens_dir = str(Path.home()) + '/Dropbox/scilens/cache/diffusion_graph/scilens_3M/'
+sciclops_dir = str(Path.home()) + '/Dropbox/sciclops/'
+############################### ######### ###############################
+
+################################ HELPERS ################################
+
+############################### ######### ###############################
+
+# def outgoing_link_based_clustering():
+articles = pd.read_csv(scilens_dir + 'article_details_v2.tsv.bz2', sep='\t')[:1000]
+G = read_graph(scilens_dir + 'diffusion_graph_v7.tsv.bz2')
+articles['refs'] = articles.url.apply(lambda u: set(G[u]))
+articles = articles.set_index('url')
+
+te = TransactionEncoder()
+te_ary = te.fit_transform(articles['refs'])
+
+clusters = apriori(pd.DataFrame(te_ary, columns=te.columns_), min_support=2/len(articles), use_colnames=True)
+
+# #all the combinations of references
+# def refs_to_clusters(refs, max_combinations=3):
+#     return [comb for c in range(max_combinations) for comb in combinations(refs, c+1)]
+
+# articles['clusters'] = articles.apply(lambda a: refs_to_clusters(a.refs), axis=1)
+
+# clusters = articles.clusters.apply(pd.Series) \
+#     .merge(articles[['clusters']], right_index = True, left_index = True) \
+#     .reset_index() \
+#     .drop(['clusters'], axis = 1) \
+#     .melt(id_vars = ['url'], value_name = 'clusters') \
+#     .drop('variable', axis = 1) \
+#     .dropna()
+
+# clusters = clusters.groupby('clusters').url.apply(list).to_frame()
+
+# clusters['length'] = clusters.url.apply(len)
 
 
-scilens_dir = scilens_dir + '/cache/diffusion_graph/scilens_3M/'
-# #articles = pd.read_csv(scilens_dir + 'article_details_v2.tsv.bz2', sep='\t')
-# #G = read_graph(scilens_dir + 'diffusion_graph_v7.tsv.bz2')
+    # # add article to clusters
+    # def add_to_clusters(article, refs, clusters, max_combinations=3):
+        
+    #     for c in range(max_combinations):
+    #         for comb in combinations(refs, c+1):
+
+    #             if clusters.loc[clusters.refs == comb].empty:
+    #                 clusters = clusters.append({'refs':comb,'articles':[article]}, ignore_index=True)
+    #             else:
+    #                 clusters.loc[clusters.refs == comb, 'articles'] = clusters.loc[clusters.refs == comb, 'articles'] + [article]
 
 
-papers = pd.read_csv(scilens_dir + 'paper_details_v1.tsv.bz2', sep='\t')
-
-# #def clean_graph(graph_file):
-
+  
+    
+  #  return clusters
