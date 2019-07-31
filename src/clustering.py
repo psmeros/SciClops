b4f7a5217a6696ca709603f78217fe784ceacaa7
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-07-31 23:01:27 +0200

    Computed cooc matrix of articles-papers.

--- src/clustering.py
+++ src/clustering.py
@@ -17,30 +17,32 @@ low_memory = True
 ############################### ######### ###############################
 
 ################################ HELPERS ################################
+def apriori_clustering():
+  te = TransactionEncoder()
+  te_ary = te.fit_transform(articles['refs'])
+
+  clusters = apriori(pd.DataFrame(te_ary, columns=te.columns_), min_support=2/len(articles), use_colnames=True, low_memory=low_memory).rename(columns={'itemsets':'refs'})
+
+
+  def get_articles_for_cluster(cluster_refs):
+    return articles[[cluster_refs.issubset(r) for r in articles.refs]].index.tolist()
+
+  clusters['articles'] = clusters.refs.apply(get_articles_for_cluster)
+  clusters['length'] = clusters.articles.apply(len)
+
 
 ############################### ######### ###############################
 
-# def outgoing_link_based_clustering():
-articles = pd.read_csv(scilens_dir + 'article_details_v2.tsv.bz2', sep='\t')[:1000]
+
+articles = pd.read_csv(scilens_dir + 'article_details_v2.tsv.bz2', sep='\t')#[:1000]
 G = read_graph(scilens_dir + 'diffusion_graph_v7.tsv.bz2')
 articles['refs'] = articles.url.apply(lambda u: set(G[u]))
 articles = articles.set_index('url')
 
+#cleaning
 blacklist_refs  = set(open(sciclops_dir + 'blacklist/sources.txt').read().splitlines())
 articles['refs'] = articles.refs.apply(lambda r: r - blacklist_refs)
 
 te = TransactionEncoder()
-te_ary = te.fit_transform(articles['refs'])
+cooc = pd.DataFrame(te.fit_transform(articles['refs']), columns=te.columns_, index=articles.index)
 
-clusters = apriori(pd.DataFrame(te_ary, columns=te.columns_), min_support=2/len(articles), use_colnames=True, low_memory=low_memory).rename(columns={'itemsets':'refs'})
-
-
-def get_articles_for_cluster(cluster_refs):
-  return articles[[cluster_refs.issubset(r) for r in articles.refs]].index.tolist()
-
-clusters['articles'] = clusters.refs.apply(get_articles_for_cluster)
-clusters['length'] = clusters.articles.apply(len)
-
-
-    
-  #  return clusters
