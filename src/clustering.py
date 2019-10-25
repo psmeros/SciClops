55615a83f870dfd6c9415653cf3210b478eec5f0
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-10-25 15:36:37 +0000

    Better cleaning.

--- src/clustering.py
+++ src/clustering.py
@@ -31,8 +31,9 @@ sciclops_dir = str(Path.home()) + '/data/sciclops/'
 nlp = spacy.load('en_core_web_lg')
 hn_vocabulary = open(sciclops_dir + 'small_files/hn_vocabulary/hn_vocabulary.txt').read().splitlines()
 
-num_clusters = 10
+NUM_CLUSTERS = 10
 embeddings_dim = 50
+CLAIM_THRESHOLD = 10
 ############################### ######### ###############################
 
 ################################ HELPERS ################################
@@ -42,45 +43,27 @@ def read_graph(graph_file):
 	return nx.from_pandas_edgelist(pd.read_csv(graph_file, sep='\t', header=None), 0, 1, create_using=nx.DiGraph())
 
 #Remove stopwords/Lemmatize
-def nlp_clean(text):
-	if not text.endswith('.'):
-		text = ''
+def clean_claim(text):
+	if (not text.endswith('.')) or ('\n' in text):
+		text = []
 	else:
-		text = ' '.join([str(w.lemma_) for w in nlp(text) if not (w.is_stop or len(w) == 1)])
+		text = [str(w.lemma_) for w in nlp(text) if not (w.is_stop or len(w) == 1)]
+
+		#remove small claims
+		if len(text) < CLAIM_THRESHOLD:
+			text = []
+		else:
+			text = [w for w in hn_vocabulary if w in text]
+	return text
+
+#Remove stopwords/Lemmatize
+def clean_paper(text):
+	text = [str(w.lemma_) for w in nlp(str(text)) if not (w.is_stop or len(w) == 1)]
 	return text
 
 
-def keywords_relation(clean_claims, partition):
-	
-	clean_claims = clean_claims.apply(lambda c: [w for w in hn_vocabulary if w in c])
-	clean_claims = clean_claims[clean_claims.apply(lambda c: len(c) > 0 and c!= [partition])]
-	
-	clean_claims[clean_claims.apply(lambda c: len(c)==1)] = clean_claims[clean_claims.apply(lambda c: len(c)==1)].apply(lambda c: c + [partition])
-	clean_claims = clean_claims.apply(lambda c: [[c[k1], c[k2]] for k1 in range(len(c)) for k2 in range(k1+1,len(c))])
-	clean_claims = pd.DataFrame(clean_claims).rename(columns={'clean_claim': 'pairs'})
-	clean_claims['weight'] = 1/clean_claims['pairs'].apply(len) 
-	clean_claims = clean_claims.explode('pairs')
-	clean_claims['k1'], clean_claims['k2'] = zip(*clean_claims['pairs'].apply(lambda p: (p[0], p[1]) if p[0] < p[1] else (p[1], p[0])))
-	clean_claims = clean_claims.drop('pairs', axis=1)
-	clean_claims = clean_claims.groupby(['k1', 'k2']).sum().reset_index().sort_values('weight', ascending=False)[:15]
-	clean_claims['k1'], clean_claims['k2'] = zip(*clean_claims.apply(lambda c: (c['k1'],c['k2']) if c['k1'] == partition else (c['k2'],c['k1']),axis=1))
-	clean_claims = clean_claims[['k2', 'weight']].set_index('k2')
-	
-	sns.set(context='paper', style='white', color_codes=True)
-	plt.figure(figsize=(2,5))
-	colorticks=[100, 200, 300, 400, 600, 1000]
-	ax = sns.heatmap(clean_claims, norm=LogNorm(clean_claims.min(), clean_claims.max()), cbar_kws={'ticks':colorticks, 'format':ScalarFormatter()}, vmin = 100, vmax=1000, cmap='copper')
-	ax.tick_params(axis='x', labelbottom=False)
-	ax.yaxis.label.set_visible(False)
-	plt.savefig(sciclops_dir+'figures/'+partition+'.png', bbox_inches='tight', transparent=True)
-	plt.show()
-	
-
-def transform_papers(papers, passage, representation):
+def transform_papers(papers, passage):
 	print('transforming papers...')
-
-	papers.title = papers.title.astype(str)
-	papers.full_text = papers.full_text.astype(str)
 	
 	if passage == 'title':
 		papers_text = papers.title
@@ -88,20 +71,11 @@ def transform_papers(papers, passage, representation):
 		papers_text = papers.title + ' ' + papers.full_text.apply(lambda w: w.split('\n')[0])
 	elif passage == 'full_text':
 		papers_text = papers.title + ' ' + papers.full_text
-
-	if representation == 'embeddings':
-		papers_vec = papers_text.parallel_apply(lambda x: nlp(x).vector).apply(pd.Series)
-		papers_vec = TruncatedSVD(2).fit_transform(papers_vec)
-		#papers_vec = GaussianMixture(num_clusters).fit(papers_vec).predict_proba(papers_vec)
-		papers_vec = pd.DataFrame(papers_vec, index=papers.index)
-	elif representation == 'bow':
-		vectorizer = TfidfVectorizer(vocabulary=hn_vocabulary).fit(papers_text)
-		papers_vec = vectorizer.transform(papers_text)
-		PCA = TruncatedSVD(embeddings_dim).fit(papers_vec)
-		papers_vec = pd.DataFrame(PCA.transform(papers_vec), index=papers.index)
-	elif representation == 'bow_embeddings':
-		papers_text = papers_text.apply(lambda t: ' '.join([w for w in hn_vocabulary if w in t]))
-		papers_vec = papers_text.parallel_apply(lambda x: nlp(x).vector).apply(pd.Series)
+	
+	papers_vec = papers_text.parallel_apply(lambda x: nlp(' '.join(clean_paper(x))).vector).apply(pd.Series).values
+	papers_vec = TruncatedSVD(NUM_CLUSTERS).fit_transform(papers_vec)
+	#papers_vec = GaussianMixture(NUM_CLUSTERS).fit(papers_vec).predict_proba(papers_vec)
+	papers_vec = pd.DataFrame(papers_vec, index=papers.index)
 
 	return papers_vec
 
@@ -109,38 +83,37 @@ def transform_claims(claims, clustering):
 	print('transforming claims...')
 
 	if clustering == 'GSDMM':
-		#clusters to one-hot (GSDMM model)
-		mgp = MovieGroupProcess(K=num_clusters, alpha=0.01, beta=0.01, n_iters=50)
+		mgp = MovieGroupProcess(K=NUM_CLUSTERS, alpha=0.01, beta=0.01, n_iters=50)
 		claims['cluster'] = mgp.fit(claims['clean_claim'], len(set([e for l in claims['clean_claim'].tolist() for e in l])))
-
-		claims_vec = np.zeros((len(claims), num_clusters))
+		claims_vec = np.zeros((len(claims), NUM_CLUSTERS))
 		claims_vec[np.arange(len(claims)), claims.cluster.to_numpy()] = 1
-	elif clustering == 'GMM':
+	elif clustering == 'PCA-GMM':
 		claims_vec = claims['clean_claim'].parallel_apply(lambda x: nlp(' '.join(x)).vector).apply(pd.Series).values
-		#claims_vec = TSNE().fit_transform(claims_vec)	
 		claims_vec = TruncatedSVD(2).fit_transform(claims_vec)	
-		claims_vec = GaussianMixture(num_clusters).fit(claims_vec).predict_proba(claims_vec)
+		claims_vec = GaussianMixture(NUM_CLUSTERS).fit(claims_vec).predict_proba(claims_vec)
+	elif clustering == 'TSNE-GMM':
+		claims_vec = claims['clean_claim'].parallel_apply(lambda x: nlp(' '.join(x)).vector).apply(pd.Series).values
+		claims_vec = TSNE().fit_transform(claims_vec)	
+		claims_vec = GaussianMixture(NUM_CLUSTERS).fit(claims_vec).predict_proba(claims_vec)
 
 	claims_vec = pd.DataFrame(claims_vec, index=claims.index)
 	return claims_vec
 
 
-def data_preprocessing(representation, clustering, partition='cancer', passage='prelude', use_cache=True):
+def data_preprocessing(clustering, passage='prelude', use_cache=True):
 	'''
 	:param representation: bow, embeddings, bow_embeddings
-	:param partition: article topic e.g., cancer
 	:param passage: title, prelude, full_text (filter for paper text)
 	'''
 	if use_cache:
 		cooc = pd.read_csv(sciclops_dir + 'cache/cooc.tsv.bz2', sep='\t', index_col=['url', 'claim'])
 		claims_vec = pd.read_csv(sciclops_dir + 'cache/claims_vec.tsv.bz2', sep='\t', index_col=['url', 'claim'])
-		papers_vec = pd.read_csv(sciclops_dir + 'cache/papers_vec_'+representation+'_'+passage+'.tsv.bz2', sep='\t', index_col='url')
+		papers_vec = pd.read_csv(sciclops_dir + 'cache/papers_vec_'+'_'+passage+'.tsv.bz2', sep='\t', index_col='url')
 
 	else:
 		pandarallel.initialize()
 		
 		articles = pd.read_csv(scilens_dir + 'article_details_v3.tsv.bz2', sep='\t')
-		#articles = pd.read_csv(sciclops_dir + 'cache/'+partition+'_articles.tsv.bz2', sep='\t')
 		claims = articles[['url', 'quotes']].drop_duplicates(subset='url')
 
 		claims.quotes = claims.quotes.parallel_apply(lambda l: list(map(lambda d: d['quote'], eval(l))))
@@ -149,24 +122,16 @@ def data_preprocessing(representation, clustering, partition='cancer', passage='
 
 		#cleaning
 		print('cleaning...')
-		claims['clean_claim'] = claims['claim'].parallel_apply(lambda c: list(set([w for w in nlp_clean(c).split()])))
-		#remove small claims
-		claims = claims[claims['clean_claim'].str.len() >= 5]
-		#remove noise
-		claims['clean_claim'] = claims['clean_claim'].parallel_apply(lambda c: [w for w in hn_vocabulary if w in c])
+		claims['clean_claim'] = claims['claim'].parallel_apply(clean_claim)
 		claims = claims[claims['clean_claim'].str.len() != 0]
 
+		G = read_graph(scilens_dir + 'diffusion_graph_v7.tsv.bz2')
+		G.remove_nodes_from(open(sciclops_dir + 'small_files/blacklist/sources.txt').read().splitlines())
+		claims['refs'] = claims.url.parallel_apply(lambda u: set(G[u]))
+		refs = [e for l in claims['refs'].to_list() for e in l]
 
 		papers = pd.read_csv(scilens_dir + 'paper_details_v1.tsv.bz2', sep='\t').drop_duplicates(subset='url')
-		G = read_graph(scilens_dir + 'diffusion_graph_v7.tsv.bz2')
-		claims['refs'] = claims.url.parallel_apply(lambda u: set(G[u]))
-
-		blacklist_refs = set(open(sciclops_dir + 'small_files/blacklist/sources.txt').read().splitlines())
-		claims['refs'] = claims.refs.parallel_apply(lambda r: (r - blacklist_refs))
-		papers = papers[papers['url'].isin([e for l in claims['refs'].to_list() for e in l])]
-
-		#plot relation
-		#keywords_relation(claims['clean_claim'], partition)
+		papers = papers[papers['url'].isin(refs)]
 
 		claims = claims.set_index(['url', 'claim'])
 		papers = papers.set_index('url')
@@ -174,13 +139,13 @@ def data_preprocessing(representation, clustering, partition='cancer', passage='
 		mlb = MultiLabelBinarizer()
 		cooc = pd.DataFrame(mlb.fit_transform(claims.refs), columns=mlb.classes_, index=claims.index)
 
-		papers_vec = transform_papers(papers, passage, representation)
+		papers_vec = transform_papers(papers, passage)
 		claims_vec = transform_claims(claims, clustering)
 
 		#caching    
 		cooc.to_csv(sciclops_dir + 'cache/cooc.tsv.bz2', sep='\t')
 		claims_vec.to_csv(sciclops_dir + 'cache/claims_vec.tsv.bz2', sep='\t')
-		papers_vec.to_csv(sciclops_dir + 'cache/papers_vec_'+representation+'_'+passage+'.tsv.bz2', sep='\t')
+		papers_vec.to_csv(sciclops_dir + 'cache/papers_vec_'+'_'+passage+'.tsv.bz2', sep='\t')
 	
 	cooc = torch.Tensor(cooc.values.astype(float))
 	claims_vec = torch.Tensor(claims_vec.values.astype(float))
@@ -191,7 +156,7 @@ def data_preprocessing(representation, clustering, partition='cancer', passage='
 
 ############################### ######### ###############################
 
-cooc, claims_vec, papers_vec = data_preprocessing('embeddings', 'GMM', use_cache=True)
+cooc, claims_vec, papers_vec = data_preprocessing('PCA-GMM', use_cache=False)
 
 
 # Hyper Parameters
@@ -211,11 +176,11 @@ class ClusterNet(nn.Module):
 			nn.Linear(2, hidden),
 			nn.BatchNorm1d(hidden),
 			nn.ReLU(),
-			nn.Linear(hidden, num_clusters),
-			nn.BatchNorm1d(num_clusters),
+			nn.Linear(hidden, NUM_CLUSTERS),
+			nn.BatchNorm1d(NUM_CLUSTERS),
 			nn.ReLU()
-			# nn.Linear(num_clusters, num_clusters),
-			# nn.BatchNorm1d(num_clusters),
+			# nn.Linear(NUM_CLUSTERS, NUM_CLUSTERS),
+			# nn.BatchNorm1d(NUM_CLUSTERS),
 			# nn.ReLU()
 		)
 		
--- src/partitioning.py
+++ src/partitioning.py
@@ -46,3 +46,29 @@ def filter_partitions(keyphrase):
 	articles = pd.read_csv(scilens_dir + 'article_details_v4.tsv.bz2', sep='\t')
 	articles = articles[articles['partition'].apply(lambda s: keyphrase in s)]
 	articles.to_csv(sciclops_dir + keyphrase + '_articles.tsv.bz2', sep='\t', index=False)
+
+def partition_relations(clean_claims, partition):
+	
+	clean_claims = clean_claims.apply(lambda c: [w for w in hn_vocabulary if w in c])
+	clean_claims = clean_claims[clean_claims.apply(lambda c: len(c) > 0 and c!= [partition])]
+	
+	clean_claims[clean_claims.apply(lambda c: len(c)==1)] = clean_claims[clean_claims.apply(lambda c: len(c)==1)].apply(lambda c: c + [partition])
+	clean_claims = clean_claims.apply(lambda c: [[c[k1], c[k2]] for k1 in range(len(c)) for k2 in range(k1+1,len(c))])
+	clean_claims = pd.DataFrame(clean_claims).rename(columns={'clean_claim': 'pairs'})
+	clean_claims['weight'] = 1/clean_claims['pairs'].apply(len) 
+	clean_claims = clean_claims.explode('pairs')
+	clean_claims['k1'], clean_claims['k2'] = zip(*clean_claims['pairs'].apply(lambda p: (p[0], p[1]) if p[0] < p[1] else (p[1], p[0])))
+	clean_claims = clean_claims.drop('pairs', axis=1)
+	clean_claims = clean_claims.groupby(['k1', 'k2']).sum().reset_index().sort_values('weight', ascending=False)[:15]
+	clean_claims['k1'], clean_claims['k2'] = zip(*clean_claims.apply(lambda c: (c['k1'],c['k2']) if c['k1'] == partition else (c['k2'],c['k1']),axis=1))
+	clean_claims = clean_claims[['k2', 'weight']].set_index('k2')
+	
+	sns.set(context='paper', style='white', color_codes=True)
+	plt.figure(figsize=(2,5))
+	colorticks=[100, 200, 300, 400, 600, 1000]
+	ax = sns.heatmap(clean_claims, norm=LogNorm(clean_claims.min(), clean_claims.max()), cbar_kws={'ticks':colorticks, 'format':ScalarFormatter()}, vmin = 100, vmax=1000, cmap='copper')
+	ax.tick_params(axis='x', labelbottom=False)
+	ax.yaxis.label.set_visible(False)
+	plt.savefig(sciclops_dir+'figures/'+partition+'.png', bbox_inches='tight', transparent=True)
+	plt.show()
+	
