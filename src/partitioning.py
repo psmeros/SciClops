aa73f36eb0d422d3159ca484616f01877655b387
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2019-11-14 02:21:19 +0100

    Keyphrase relations plotting.

--- src/partitioning.py
+++ src/partitioning.py
@@ -1,15 +1,18 @@
 import string
 from pathlib import Path
 
-import pandas as pd
-from nltk.corpus import stopwords
-from pandarallel import pandarallel
-import seaborn as sns
 import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+import pke
+import seaborn as sns
 from matplotlib.colors import LogNorm
 from matplotlib.ticker import ScalarFormatter
+from nltk.corpus import stopwords
+from pandarallel import pandarallel
 
-import pke
+from matrix_preparation import clean_claim, read_graph
 
 ############################### CONSTANTS ###############################
 scilens_dir = str(Path.home()) + '/data/scilens/cache/diffusion_graph/scilens_3M/'
@@ -19,7 +22,6 @@ hn_vocabulary = open(sciclops_dir + 'small_files/hn_vocabulary/hn_vocabulary.txt
 ############################### ######### ###############################
 
 ################################ HELPERS ################################
-
 def keyphrase_extraction(text, n=10):	
 	extractor = pke.unsupervised.MultipartiteRank()
 	extractor.load_document(input=text)
@@ -34,10 +36,6 @@ def keyphrase_extraction(text, n=10):
 	keyphrases = extractor.get_n_best(n)
 	return keyphrases
 
-############################### ######### ###############################
-
-pandarallel.initialize()
-
 def extract_keyphrases():
 	articles = pd.read_csv(scilens_dir + 'article_details_v3.tsv.bz2', sep='\t')
 	articles['keyphrases'] = articles.apply(lambda a: keyphrase_extraction(str(a.title) + ' ' + str(a.full_text)), axis=1)
@@ -46,34 +44,50 @@ def extract_keyphrases():
 	articles['partition'] = articles.keyphrases.apply(lambda l: set([kph for sl in [k.split() for (k, _) in l] for kph in sl if kph in ght]))
 	articles.to_csv(scilens_dir + 'article_details_v4.tsv.bz2', sep='\t', index=False)
 
+############################### ######### ###############################
 
-def filter_partitions(keyphrase):
-	articles = pd.read_csv(scilens_dir + 'article_details_v4.tsv.bz2', sep='\t')
-	articles = articles[articles['partition'].apply(lambda s: keyphrase in s)]
-	articles.to_csv(sciclops_dir + keyphrase + '_articles.tsv.bz2', sep='\t', index=False)
+def keyphrase_relations(keyphrase, blacklist):
+	pandarallel.initialize()
+	
+	articles = pd.read_csv(scilens_dir + 'article_details_v3.tsv.bz2', sep='\t')
+	claims = articles[['url', 'quotes']].drop_duplicates(subset='url')
 
-def partition_relations(clean_claims, partition):
-	
-	clean_claims = clean_claims.apply(lambda c: [w for w in hn_vocabulary if w in c])
-	clean_claims = clean_claims[clean_claims.apply(lambda c: len(c) > 0 and c!= [partition])]
-	
-	clean_claims[clean_claims.apply(lambda c: len(c)==1)] = clean_claims[clean_claims.apply(lambda c: len(c)==1)].apply(lambda c: c + [partition])
-	clean_claims = clean_claims.apply(lambda c: [[c[k1], c[k2]] for k1 in range(len(c)) for k2 in range(k1+1,len(c))])
-	clean_claims = pd.DataFrame(clean_claims).rename(columns={'clean_claim': 'pairs'})
-	clean_claims['weight'] = 1/clean_claims['pairs'].apply(len) 
-	clean_claims = clean_claims.explode('pairs')
-	clean_claims['k1'], clean_claims['k2'] = zip(*clean_claims['pairs'].apply(lambda p: (p[0], p[1]) if p[0] < p[1] else (p[1], p[0])))
-	clean_claims = clean_claims.drop('pairs', axis=1)
-	clean_claims = clean_claims.groupby(['k1', 'k2']).sum().reset_index().sort_values('weight', ascending=False)[:15]
-	clean_claims['k1'], clean_claims['k2'] = zip(*clean_claims.apply(lambda c: (c['k1'],c['k2']) if c['k1'] == partition else (c['k2'],c['k1']),axis=1))
-	clean_claims = clean_claims[['k2', 'weight']].set_index('k2')
-	
+	G = read_graph(scilens_dir + 'diffusion_graph_v7.tsv.bz2')
+	G.remove_nodes_from(open(sciclops_dir + 'small_files/blacklist/sources.txt').read().splitlines())
+
+	claims.quotes = claims.quotes.parallel_apply(lambda l: list(map(lambda d: d['quote'], eval(l))))
+	claims = claims.explode('quotes').rename(columns={'quotes': 'claim'})
+	claims = claims[~claims['claim'].isna()]
+
+	claims = claims[claims['claim'].str.contains(keyphrase)]
+
+	claims['clean_claim'] = claims['claim'].parallel_apply(clean_claim)
+	claims = claims[claims['clean_claim'].str.len() != 0]
+
+	tweets = pd.read_csv(scilens_dir + 'tweet_details_v1.tsv.bz2', sep='\t').drop_duplicates(subset='url').set_index('url')
+	claims['popularity'] = claims.url.parallel_apply(lambda u: sum([tweets.loc[t]['popularity'] for t in G.predecessors(u) if t in tweets.index]))
+
+	claims['popularity'] = claims['popularity'] * 1/claims['clean_claim'].apply(len)
+
+	claims = claims[['clean_claim', 'popularity']]
+	claims = claims.explode('clean_claim')
+	claims = claims[~claims['clean_claim'].isin(blacklist)]
+
+	claims = claims.groupby(['clean_claim']).sum().reset_index().sort_values('popularity', ascending=False)[:15]
+
+	claims = claims.rename(columns={'clean_claim':'health and nutrition terms'})
+	claims = claims.set_index('health and nutrition terms')
+
 	sns.set(context='paper', style='white', color_codes=True)
 	plt.figure(figsize=(2,5))
-	colorticks=[100, 200, 300, 400, 600, 1000]
-	ax = sns.heatmap(clean_claims, norm=LogNorm(clean_claims.min(), clean_claims.max()), cbar_kws={'ticks':colorticks, 'format':ScalarFormatter()}, vmin = 100, vmax=1000, cmap='copper')
-	ax.tick_params(axis='x', labelbottom=False)
-	ax.yaxis.label.set_visible(False)
-	plt.savefig(sciclops_dir+'figures/'+partition+'.png', bbox_inches='tight', transparent=True)
+	#colorticks=[1, 50, 100]
+	#ax = sns.heatmap(claims, norm=LogNorm(claims['popularity'].min(), claims['popularity'].max()), cbar_kws={'ticks':colorticks, 'format':ScalarFormatter()}, vmin = 1, vmax=250, cmap='copper')
+	ax = sns.heatmap(claims, cmap='copper')
+	# ax.tick_params(axis='x', labelbottom=False)
+	# ax.yaxis.label.set_visible(False)
+	plt.savefig(sciclops_dir+'figures/'+keyphrase+'.png', bbox_inches='tight', transparent=True)
 	plt.show()
 	
+
+if __name__ == "__main__":
+	keyphrase_relations('meat', ['meat', 'beef', 'pork', 'chicken', 'TMAO', 'lamb'])
